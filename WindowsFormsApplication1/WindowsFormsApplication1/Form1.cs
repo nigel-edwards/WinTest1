f629a8e79c2c6fb71f2ca2e968939fd2502295e6
253737 <253737@H0817>
2017-08-31 10:30:44 +0100

    Added another comment

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -25,6 +25,7 @@ namespace WindowsFormsApplication1
         private void Form1_Load(object sender, EventArgs e)
         {
             // This is a comment
+            // So is this :-)
         }
     }
 }
