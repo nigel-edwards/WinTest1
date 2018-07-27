2f6324dc5bc55cc826c949de3e6c0b5c6660bc70
253737 <253737@H0817>
2018-07-27 13:30:23 +0100

    Just testing

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void label1_Click(object sender, EventArgs e)
         {
-
+            // yet another comment
         }
 
         private void Form1_Load(object sender, EventArgs e)
